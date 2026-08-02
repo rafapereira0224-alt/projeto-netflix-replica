4a6e045c3fa647827ba1f3bc81a152d0151456e9
Rafael <rafa.pereira0224@gmail.com>
2026-08-02 10:27:26 -0300

    Adiciona Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
-  <App />,
-  <Analytics />
+  <>
+    <App />
+    <Analytics />
+  </>
 )
